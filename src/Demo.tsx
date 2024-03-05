de0f4c688a9165241ad40a45834d17f9e7028d32
DLBell217 <157739853+DLBell217@users.noreply.github.com>
2024-03-05 13:54:03 -0500

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Button from '@mui/joy/Button';
 import Box from '@mui/joy/Box';
+import Add from '@mui/icons-material/Add';
 
 export default function BasicButtons() {
   return (
