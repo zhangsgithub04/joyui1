add5606e39d3a8873bb690bfea80632ec74f1cd0
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-03-05 14:12:04 -0500

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,17 +1,51 @@
 import * as React from 'react';
 import Button from '@mui/joy/Button';
 import Box from '@mui/joy/Box';
++import FormControl from '@mui/joy/FormControl';
++import FormLabel from '@mui/joy/FormLabel';
++import FormHelperText from '@mui/joy/FormHelperText';
++import Input from '@mui/joy/Input';
+
 
 export default function BasicButtons() {
   return (
+    <Box>
     <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
       <Button>Button</Button>
       <Button disabled>Disabled</Button>
       <Button loading>Loading</Button>
 
        <Button>Click me</Button>
-
+    
       
     </Box>
+    
+    <Box>
+    +<FormControl
+  id="Id"
+  required
+  size="sm"
+  color="primary">
+  <FormLabel>
+    Label
+  </FormLabel>
+  <Input
+    placeholder="Placeholder"
+    name="Name"
+    type="tel"
+    autoComplete="on"
+    autoFocus
+    error
+    fullWidth
+    defaultValue="DefaultValue"
+    variant="outlined" />
+  <FormHelperText>
+    Help!
+  </FormHelperText>
+</FormControl>
+
+    </Box>
+
+</Box>
   );
 }
