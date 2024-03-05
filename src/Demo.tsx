be3d7647f725d537de6b43ed662fa91a3924c262
DLBell217 <157739853+DLBell217@users.noreply.github.com>
2024-03-05 14:03:29 -0500

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Button from '@mui/joy/Button';
 import Box from '@mui/joy/Box';
-import Add from '@mui/icons-material/Add';
 
 export default function BasicButtons() {
   return (
@@ -9,7 +8,7 @@ export default function BasicButtons() {
       <Button>Button</Button>
       <Button disabled>Disabled</Button>
       <Button loading>Loading</Button>
-      <Button variant="outlined" startDecorator={<Add />}>bruh</Button>
+      <Button>Click me</Button>
     </Box>
   );
 }
