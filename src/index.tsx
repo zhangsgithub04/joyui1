d92f997043b00e8895e19ea3dfb6a296c296e87b
zhangsgithub04 <ssen_zhang@hotmail.com>
2024-03-07 14:14:03 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import Demo2 from './Demo2';
 
 import { StepsSlider } from './Component2';
 
+
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
