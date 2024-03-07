9de077a862aa1ef11a65772baf4c59fad8228864
zhangsgithub04 <ssen_zhang@hotmail.com>
2024-03-07 13:36:18 -0500

    Update Demo2.tsx

--- src/Demo2.tsx
+++ src/Demo2.tsx
@@ -1,1 +1,16 @@
 
+import * as React from 'react';
+import Button from '@mui/joy/Button';
+import Box from '@mui/joy/Box';
+
+export default function Demo2() {
+  return (
+    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
+      <Button>Button safsadfsaf yuyuuu </Button>
+      <Button disabled>Disabled</Button>
+      <Button loading>Loading</Button>
+      <Button >Click me</Button>
+      
+    </Box>
+  );
+}
