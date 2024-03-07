1d48804d9e806262c8a7ede56e920ee2a89a398d
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-03-07 13:47:46 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
 import Demo from './Demo';
+//import Demo from './Demo';
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <CssVarsProvider>
         <Demo />
+        <Demo />
+        <Demo2 />
+        <Demo2 />
+        
       </CssVarsProvider>
     </StyledEngineProvider>
   </React.StrictMode>
