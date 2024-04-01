ec907b187260f23fb65b6015b2ab1b74f13e73c3
Jack Scali <137472325+jackscali1@users.noreply.github.com>
2024-04-01 11:38:36 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,32 +2,21 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
 import SignUp from './SignUp'; // Import SignUp component
-import Demo from './Demo';
-import Demo2 from './Demo2';
-import Component2 from './Component2';
-import App from './App';  // Ensure this is the correct path to your App component
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 // Get the root element to render your app
-const rootElement = document.querySelector("#root");
+const rootElement = document.getElementById('root');
 
 if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
         <React.StrictMode>
             <StyledEngineProvider injectFirst>
                 <CssVarsProvider>
-                    <SignUp /> {/* Add the SignUp component */}
-                    <Demo />
-                    <Demo2 />
-                    <Component2 />
+                    <SignUp /> {/* Render the SignUp component directly */}
                 </CssVarsProvider>
             </StyledEngineProvider>
         </React.StrictMode>
     );
 }
+
 
