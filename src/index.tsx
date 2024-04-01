aa0191e01c69cbe53d00fa257bdc3cf94f917d8b
Jack Scali <137472325+jackscali1@users.noreply.github.com>
2024-04-01 18:28:19 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import SignUp from './SignUp'; 
 import SignIn from './SignIn';
 import MainInterface from './MainInterface';
@@ -10,10 +11,19 @@ if (rootElement) {
     const root = ReactDOM.createRoot(rootElement);
     root.render(
         <React.StrictMode>
-            <SignUp /> {}
-            <SignIn /> {}
-            <MainInterface /> {}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<SignUp />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/main" element={<MainInterface />} />
+                </Routes>
+                {/* Link to the MainInterface */}
+                <footer style={{textAlign: 'center', marginTop: '20px'}}>
+                    <Link to="/main">Go to Main Interface</Link>
+                </footer>
+            </BrowserRouter>
         </React.StrictMode>
     );
 }
+
 
